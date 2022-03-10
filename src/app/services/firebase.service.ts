07fcaf43fc57f8c9a0b997824add97a0ade6a30b
Víctor Romário <victor.romario.seriado@gmail.com>
2022-03-10 16:41:43 -0300

    style: remove more consoles logs

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -33,7 +33,6 @@ export class FirebaseService {
   }
 
   login(emailValue: any, passwordValue: any) {
-    console.log('fiz login')
     return new Promise((resolve, reject) => {
       this.fireauth.signInWithEmailAndPassword(emailValue, passwordValue).then(
         res => resolve(res),
