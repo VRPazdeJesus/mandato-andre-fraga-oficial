d637fce9f60ecedc6235d169970c951eed26db62
Víctor Romário <victor.romario.seriado@gmail.com>
2022-06-02 01:26:12 -0300

    feat: get projetcs from database

--- src/app/projeto-de-lei/projeto-de-lei.page.ts
+++ src/app/projeto-de-lei/projeto-de-lei.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { FirebaseService } from '../services/firebase.service';
 
 @Component({
   selector: 'app-projeto-de-lei',
@@ -8,13 +9,36 @@ import { NavController } from '@ionic/angular';
 })
 export class ProjetoDeLeiPage implements OnInit {
 
-  constructor(private nav: NavController) { }
+  private contentProject: any
+  private projects: any = []
+
+  constructor(
+    private nav: NavController,
+    private firebaseService: FirebaseService
+  ) { }
 
   ngOnInit() {
+    this.getProject()
   }
 
   proximo(values:any) {
     this.nav.navigateForward('/'+values);
+  }
+
+  // get projects from database
+  getProject() {
+    this.firebaseService.getContentPage('projeto-de-lei').then(res => {
+      this.contentProject = res
+    }).then(() => {
+      for (let key in this.contentProject) {
+        if(this.contentProject[key].status === "active") {
+          this.projects.push(this.contentProject[key])
+        }
+      }
+    }).then(() => {
+      console.log('contentProject ', this.contentProject)
+      console.log('projects', this.projects)
+    })
   }
 
 }
